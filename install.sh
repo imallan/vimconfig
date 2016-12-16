5fc57708ec277675fc7ed99e6174ebab37fb97cb
allan <allan1st@gmail.com>
2016-12-16 11:49:57 +0000

    soft-link

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln vimrc ~/.vimrc
-ln vimrc.bundles ~/.vimrc.bundles
-ln gvimrc ~/.gvimrc
+ln -s "$(pwd)/vimrc" ~/.vimrc
+ln -s "$(pwd)/vimrc.bundles" ~/.vimrc.bundles
+ln -s "$(pwd)/gvimrc" ~/.gvimrc
 
 ln -s "$(pwd)/Snips" ~/.vim/Snips
 
